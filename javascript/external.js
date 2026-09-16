4fa0f800a153d73e05e53bb87a8b4265ab7f81b5
Emily Moxley <uqj9@txstate.edu>
2026-09-16 12:49:50 -0500

    Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,23 +1,15 @@
-// document.write("Where is my JS code?");
-// window.alert(5 + 6);
-// window.alert('5 + 6');
+document.writeln("<button onclick='condition()'>Conditional Test</button>");
 
-// document.write('<h1>A heading</h1>');
-// document.write('<p>A sentence.</p>');
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
 
-// const x = "web";
-// const y = "mapping";
-// const out = x + y;
-// document.write(out);
-
-// A prompt box is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-
-a = 22;
-const b = 33;
-document.writeln(a + b);
-document.writeln("<br>");
-document.writeln(a += b);
-document.writeln("<br>");
-document.writeln(a += b);
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+    document.writeln("You chose Cancel!");
+  }
+}
