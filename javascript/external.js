5767c5a36ff0e83946569ecadcbd7ce33c620889
Emily Moxley <uqj9@txstate.edu>
2026-09-16 13:52:41 -0500

    Saved username

--- javascript/external.js
+++ javascript/external.js
@@ -1,6 +1,1 @@
-arr = ["Mael","Eric","Randall","James"];
-
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
-}
+user_name = window.prompt("Please enter your name", "Type your name here")
