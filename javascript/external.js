25b02428d12247aa5cb4131a602b291934f6d236
Emily Moxley <uqj9@txstate.edu>
2026-09-23 09:34:49 -0500

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,2 +1,15 @@
-user_name = window.prompt("Please enter your name", "Type your name here")
-document.getElementById("welcome-header").innerHTML = "Hello, welcome " + user_name + "!";
+ //user_name = window.prompt("Please enter your name", "Type your name here")
+ //document.getElementById("welcome-header").innerHTML = "Hello, welcome " + user_name + "!";
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+
+  // A prompt box is used to promt users toinput a value before entering a page.
+  user_name = window.prompt(a,b);
+
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>";
+
+  return message;
+}
+document.write(welcome());
